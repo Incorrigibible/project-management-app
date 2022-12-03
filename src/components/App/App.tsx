c7dcd0b273466b5ef3de2ac545c1b05331f16ded
Oleg Luppov <lupperpuper@gmail.com>
2022-12-04 02:19:57 +0400

    feat: create a project structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import './app.scss'
+
+export const App: React.FC = () => {
+  return <Box component="div" className="container" />
+}

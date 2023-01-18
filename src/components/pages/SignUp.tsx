617eff2006728898111009bd7d016fe5f3227390
ARTEM <artem-mac@ARTEMs-MacBook-Air.local>
2023-01-19 02:01:39 +0800

    initial commit

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -2,11 +2,11 @@ import { Box } from "@mui/material";
 import React from "react";
 import { Title } from "../Titles/Title";
 
-export const SignUp:React.FC = () => {
+export const SignUp: React.FC = () => {
 
- return (
-    <Box component='form' >
-        <Title title={'Создайте аккаунт'}  selector={'title'}/>
-    </Box>
- )
+    return (
+        <Box component='form' >
+            <Title title={'Зарегистрируйся'} selector={'title'} />
+        </Box>
+    )
 }
